9ab3fca3305bc18d802f5195044ca3b86b53d63b
Pascal Hartig <phartig@rdrei.net>
2016-06-25 12:57:55 +0100

    Add tests

--- src/Options/Applicative/Text.hs
+++ src/Options/Applicative/Text.hs
@@ -1,3 +1,5 @@
+-- | This library provides parser builders akin to what optparse-applicative
+-- offers for 'String' out of the Box, but for 'Data.Text'.
 module Options.Applicative.Text
   ( text
   , textOption
@@ -7,11 +9,14 @@ import qualified Data.Text                 as T
 import qualified Options.Applicative       as Opt
 import qualified Options.Applicative.Types as Opt
 
+-- | Text 'Option' reader.
 text :: Opt.ReadM T.Text
 text = T.pack <$> Opt.readerAsk
 
+-- | Builder for an option taking a 'Data.Text' argument.
 textOption :: Opt.Mod Opt.OptionFields T.Text -> Opt.Parser T.Text
 textOption = Opt.option text
 
+-- | Builder for a 'Text' argument.
 textArgument :: Opt.Mod Opt.ArgumentFields T.Text -> Opt.Parser T.Text
 textArgument = Opt.argument text
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import qualified Data.Text                 as T
+import qualified Options.Applicative       as Opt
+import           Options.Applicative.Extra (execParserPure, getParseResult)
+import qualified Options.Applicative.Text  as OptT
 import           Test.Hspec
+
+data Options = Options { text    :: T.Text
+                       , textArg :: T.Text
+                       , textOpt :: T.Text }
+  deriving (Eq, Show)
+
+optParser :: Opt.Parser Options
+optParser = Options <$> Opt.argument OptT.text ( Opt.metavar "TEXT" )
+                 <*> OptT.textArgument ( Opt.metavar "TEXT2" )
+                 <*> OptT.textOption ( Opt.long "textopt" )
+
+parser :: Opt.ParserInfo Options
+parser = Opt.info (Opt.helper <*> optParser) mempty
 
 main :: IO ()
 main = hspec .
   describe "optparse-text" $ do
-    it "fails this test" $ do
-      True `shouldBe` False
+    let parse args = getParseResult $ execParserPure Opt.defaultPrefs parser args
+
+    it "parses to text arguments" $
+      parse ["arg1", "arg2", "--textopt", "hello world"] `shouldBe`
+        Just Options {text = "arg1", textArg = "arg2", textOpt = "hello world"}
+
+    it "parses unicode" $
+      parse ["🎈", "nø tû brëxit", "--textopt", "Benno Fünfstück"] `shouldBe`
+        Just Options {text = "🎈", textArg = "nø tû brëxit", textOpt = "Benno Fünfstück"}
